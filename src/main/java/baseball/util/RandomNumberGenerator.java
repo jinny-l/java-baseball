32fd39e17bdc8642a829ebef634cd52075e632b5
jinny-l <jinny.l.dev@gmail.com>
2022-11-30 19:57:20 +0900

    refactor: 랜덤 숫자 생성기에서 Baseball을 리턴하도록 수정

--- src/main/java/baseball/util/RandomNumberGenerator.java
+++ src/main/java/baseball/util/RandomNumberGenerator.java
@@ -1,35 +1,30 @@
 package baseball.util;
 
+import baseball.constant.BaseballNumber;
+import baseball.domain.Baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumberGenerator {
 
-    public static final int SIZE = 3;
-    private static final int START_INCLUSIVE = 1;
-    private static final int END_INCLUSIVE = 9;
+    public static final int SIZE = BaseballNumber.LENGTH.getNumber();
+    private static final int START_INCLUSIVE = BaseballNumber.START_INCLUSIVE.getNumber();
+    private static final int END_INCLUSIVE = BaseballNumber.END_INCLUSIVE.getNumber();
 
-    private final List<Integer> threeUniqueNumbers = new ArrayList<>(SIZE);
-
-    public List<Integer> getNumbers() {
-        return getThreeUniqueNumbers();
+    public Baseball getBaseball() {
+        return new Baseball(generate());
     }
 
-    private List<Integer> getThreeUniqueNumbers() {
-        int randomNumber;
-
-        while (threeUniqueNumbers.size() < SIZE) {
-            randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
-
-            if (threeUniqueNumbers.contains(randomNumber)) {
+    private List<Integer> generate() {
+        List<Integer> baseball = new ArrayList<>();
+        while (baseball.size() < SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            if (baseball.contains(randomNumber)) {
                 continue;
             }
-
-            threeUniqueNumbers.add(randomNumber);
+            baseball.add(randomNumber);
         }
-
-        return threeUniqueNumbers;
+        return baseball;
     }
-
 }
